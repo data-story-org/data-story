25df093c2a29b757403d7ede9ff8d502aedd36b6
Lenivaya <xocada@gmail.com>
2021-07-14 10:24:16 +0300

    Delete all repeating modules

--- src/core/utils/Str.ts
+++
@@ -1,9 +0,0 @@
-export const trim = (s, c) => {
-  if (c === ']') c = '\\]';
-  if (c === '^') c = '\\^';
-  if (c === '\\') c = '\\\\';
-  return s.replace(
-    new RegExp('^[' + c + ']+|[' + c + ']+$', 'g'),
-    '',
-  );
-};
