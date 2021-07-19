d3a33e1a8014963a1307d8f50bc086c6d9aa2ab7
Lenivaya <xocada@gmail.com>
2021-07-20 01:00:42 +0300

    Finalize fuzzy-search and on enter submission

--- src/core/components/controls/NodeSearch/NodeSearch.tsx
+++ src/core/components/controls/NodeSearch/NodeSearch.tsx
@@ -5,7 +5,6 @@ import React, {
   useRef,
 } from 'react';
 import { observer } from 'mobx-react';
-var Mousetrap = require('mousetrap');
 import { cloneDeep } from 'lodash';
 import { Store } from '../../../store/';
 import NodeListItem from './NodeListItem';
@@ -18,60 +17,34 @@ interface Props {
 
 const NodeSearch: FC<Props> = ({ store, onFinish }) => {
   const [search, setSearch] = useState('');
-  const [filteredNodes, setFilteredNodes] = useState(
-    store.diagram.availableNodes,
-  );
 
   const nameInput = useRef(null);
 
   useEffect(() => {
     nameInput.current.focus();
-    // Not Implemented
-    /* Mousetrap.bind('enter', handleSelect()) */
-
-    // Mousetrap.bind(Array.from('abcdefghijklmnopqrstuvwxyz'),
-    //     (key) => {
-    // 		console.log(key)
-    // 		// this.setState({
-    // 		// 	search: e.target.value
-    // 		// })
-
-    // 	}
-    // );
-    //
-    return () => {
-      Mousetrap.unbind('enter');
-    };
-  }, []);
+  });
 
   // Fuzzy-search
-  const fuse = new Fuse(store.diagram.availableNodes, {
-    keys: ['category.name.summary'],
-    findAllMatches: true,
-    ignoreLocation: true,
-    useExtendedSearch: true,
-    threshold: 0.8,
+  const nodes = store.diagram.availableNodes;
+  const [filteredNodes, setFilteredNodes] = useState(nodes);
+  const fuse = new Fuse(nodes, {
+    keys: ['name', 'category', 'summary'],
   });
 
   const searchChange = (e) => {
     setSearch(e.target.value);
 
-    const searchPattern = search.replace(/\s/g, '');
-    setFilteredNodes(
-      fuse.search(
-        `${searchPattern} | '${searchPattern} | ^${searchPattern}' | .${searchPattern}`,
-      ),
-    );
+    const searchResults = fuse
+      .search(search)
+      .map((result) => {
+        return result.item;
+      });
+    setFilteredNodes(searchResults);
   };
 
-  const handleSelect = (e) => {
-    e.preventDefault();
-
-    const name = e.target.getAttribute(
-      'data-node-model-variation-name',
-    );
+  const handleSelect = (nodeName: string) => {
     const nodeData = store.diagram.availableNodes.find(
-      (node) => node.name == name,
+      (node) => node.name === nodeName,
     );
     store.addNode(
       // Ensure the parameters will not be shared between two nodes of same type
@@ -81,22 +54,31 @@ const NodeSearch: FC<Props> = ({ store, onFinish }) => {
     onFinish();
   };
 
+  const handleInputSubmit = (e) => {
+    const nodeName = filteredNodes[0].name;
+    handleSelect(nodeName);
+  };
+
   return (
     <div className="flex flex-col bg-gray-100 -m-5 rounded shadow max-w-xl font-mono text-xs">
-      <input
-        id="node-search"
-        value={search}
-        onChange={searchChange}
-        ref={nameInput}
-        className="w-full p-4 rounded mb-4"
-        placeholder="model | method | reader | writer ..."
-        tabIndex={1}
-      />
+      <form onSubmit={handleInputSubmit}>
+        <input
+          id="node-search"
+          value={search}
+          onChange={searchChange}
+          type="text"
+          ref={nameInput}
+          className="w-full p-4 rounded mb-4 mousetrap"
+          placeholder="model | method | reader | writer ..."
+          tabIndex={1}
+        />
+      </form>
       <ul className="divide-y divide-gray-300">
         {filteredNodes.map((node) => {
           return (
             <NodeListItem
               node={node}
+              key={node.category + node.name + node.summary}
               handleSelect={handleSelect}
             />
           );
