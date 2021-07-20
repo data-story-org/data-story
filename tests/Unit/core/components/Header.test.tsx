90e9c52d87a54a43bdf786e82a45eac5ed799062
Lenivaya <xocada@gmail.com>
2021-07-20 19:19:40 +0300

    Add test for header

---
+++ tests/Unit/core/components/Header.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import Header from '../../../../src/core/components/Header';
+
+describe('<Header />', () => {
+  window.config = {
+    appName: 'DataStoryTest',
+    appDesc: 'Test',
+  };
+
+  it('Renders correctly', async () => {
+    const { findByText } = render(<Header />);
+
+    expect(await findByText(window.config.appName))
+      .toBeInTheDocument;
+    expect(await findByText(window.config.appDesc))
+      .toBeInTheDocument;
+  });
+});
--- tests/Unit/core/components/controls/NodeSearch.test.tsx
+++ tests/Unit/core/components/controls/NodeSearch.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 
