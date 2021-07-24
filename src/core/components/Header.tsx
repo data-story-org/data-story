f46b7147423839f1b8f270ca6374f429fff51c29
Lenivaya <xocada@gmail.com>
2021-07-24 23:27:52 +0300

    Use font-normal in header

--- src/core/components/Header.tsx
+++ src/core/components/Header.tsx
@@ -7,7 +7,7 @@ const Header: FC = () => {
       <div className="w-full p-4 bg-gray-700 font-sans shadow shadow-lg">
         <span className="text-xl text-malibu-500 font-medium subpixel-antialiased">
           {window.config.appName}
-          <span className="ml-2 text-sm text-gray-400 font-light subpixel-antialiased">
+          <span className="ml-2 text-sm text-gray-400 font-normal subpixel-antialiased">
             {window.config.appDesc}
           </span>
         </span>
