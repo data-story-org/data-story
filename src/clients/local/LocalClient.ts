8ef74557e0a9652dbce6a14c975dd5f6192e0005
Anders Jurisoo <jurisoo@hotmail.com>
2021-08-01 16:30:26 +0200

    Fix some types

--- src/clients/local/LocalClient.ts
+++ src/clients/local/LocalClient.ts
@@ -2,21 +2,20 @@ import { LocalServer } from './LocalServer'
 import DiagramModel from '../../diagram/models/DiagramModel';
 import ClientInterface from '../ClientInterface';
 import context from './localSampleContext'
+import { BootPayload } from '../../../../core/lib/src/types/BootPayload';
 
 const server = new LocalServer(context);
 
-export default class LocalClient
-  implements ClientInterface
-{
-  boot() {
+export default class LocalClient implements ClientInterface {
+  boot(): Promise<BootPayload> {
     return server.boot();
   }
 
-  run(model: DiagramModel) {
+  run(model: DiagramModel): Promise<{}> {
     return server.run(model.serialize());
   }
 
-  save(name: string, model: DiagramModel) {
+  save(name: string, model: DiagramModel): Promise<{}> {
     return server.save(name, model);
   }
 }
