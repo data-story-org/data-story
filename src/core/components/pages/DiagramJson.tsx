297958925457f8e796f3f4eadcc846b14af27518
Lenivaya <xocada@gmail.com>
2021-08-01 15:00:22 +0300

    Add syntax highlighting

--- src/core/components/pages/DiagramJson.tsx
+++ src/core/components/pages/DiagramJson.tsx
@@ -2,23 +2,26 @@ import React, { FC } from 'react';
 import { observer } from 'mobx-react';
 import { Store } from '../../store';
 import { nonCircularJsonStringify } from '@data-story-org/core';
+import { CodeHighlighter } from '../../utils/CodeHighlighter';
 
 interface Props {
   store: Store;
 }
 
-const DiagramJson: FC<Props> = ({store}) => {
+const DiagramJson: FC<Props> = ({ store }) => {
   return (
     <div className="h-85vh">
-      <div className="p-4 h-full">
-				<textarea
-					className="p-8 w-full h-full font-mono bg-gray-300 text-xs"
-					defaultValue={nonCircularJsonStringify(store.diagram.engine.model.serialize(), null, 4)}
-					disabled
-				/>
-			</div>
+      <div className="p-4 w-full h-full ">
+        <CodeHighlighter
+          code={nonCircularJsonStringify(
+            store.diagram.engine.model.serialize(),
+            null,
+            4,
+          )}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default observer(DiagramJson);
