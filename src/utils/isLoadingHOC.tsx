b968a76b87cec1b8d37cab00788e4796654fca3b
Lenivaya <xocada@gmail.com>
2021-08-07 20:52:04 +0300

    add withLoading HOC

---
+++ src/utils/isLoadingHOC.tsx
@@ -0,0 +1,47 @@
+import React, { useState, FC, ComponentType } from 'react';
+import ScaleLoader from 'react-spinners/ScaleLoader';
+import FadeIn from 'react-fade-in';
+
+export const AnimationLoader = () => {
+  return (
+    <div className="loading-wrapper" id="loading">
+      <FadeIn>
+        <ScaleLoader color="#000000" />
+      </FadeIn>
+    </div>
+  );
+};
+
+export interface withLoadingProps {
+  setLoading: (loading: boolean) => void;
+}
+
+export const withLoading =
+  <P extends object>(
+    WrappedComponent: ComponentType<P & withLoadingProps>,
+  ) =>
+  ({ ...props }: P) => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    const setLoadingState = (
+      isComponentLoading: boolean,
+    ) => {
+      setIsLoading(isComponentLoading);
+    };
+
+    const blur = {
+      filter: 'blur(3px)',
+    };
+
+    return (
+      <>
+        <div style={isLoading ? blur : {}}>
+          <WrappedComponent
+            {...(props as P)}
+            setLoading={setLoadingState}
+          />
+        </div>
+        {isLoading && <AnimationLoader />}
+      </>
+    );
+  };
