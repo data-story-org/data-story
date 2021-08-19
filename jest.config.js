1b512d4e585fb2c665474193ea6936511c3859d5
Lenivaya <xocada@gmail.com>
2021-08-19 09:42:06 +0300

    fix errors with running jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,19 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts'],
   transformIgnorePatterns: [
-    // '/node_modules/?!(@data-story-org/core).+js$',
     '/node_modules/(?!@data-story-org/core)',
   ],
   transform: {
     '\\.js$': 'babel-jest',
+  },
+  // FIXME remove this when jest will
+  // support package.json "exports"
+  // https://github.com/facebook/jest/issues/9771
+  // https://github.com/facebook/jest/issues/10422
+  moduleNameMapper: {
+    '^@data-story-org/core/(.*)$':
+      '@data-story-org/core/lib/src/$1',
   },
 };
