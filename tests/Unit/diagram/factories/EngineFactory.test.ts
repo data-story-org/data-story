054a17ad16a3fe331ddc0370185be517fd34203b
Anders Jurisoo <jurisoo@hotmail.com>
2021-09-05 19:01:15 +0200

    Prepare for demos

---
+++ tests/Unit/diagram/factories/EngineFactory.test.ts
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { DiagramEngine } from '@projectstorm/react-diagrams';
+import { EngineFactory } from '../../../../src/diagram/factories';
+// import { DiagramBuilder } from '@data-story-org/core'
+
+describe('EngineFactory', () => {
+  it('can make a default engine', () => {
+    expect(EngineFactory.loadOrCreate(null)).toBeInstanceOf(DiagramEngine)
+  });
+
+  // it('can make a engine with headless model', () => {
+	// 	const diagram = DiagramBuilder.begin()/* add some nodes here */.finish()
+	// 	const serializedDiagram = diagram.serialize()
+	// 	const engine = EngineFactory.loadOrCreate(serializedDiagram)
+
+  //   expect(engine).toBeInstanceOf(DiagramEngine)
+  // });	
+});
