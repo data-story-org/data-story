7c90fca769d2a398b387bd75d2dab3042ca1c999
Lenivaya <xocada@gmail.com>
2021-09-21 22:13:47 +0300

    use DataStory widget in Open modal

--- src/components/modals/Open/OpenBody.tsx
+++ src/components/modals/Open/OpenBody.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { Store } from '../../../store';
 import { observer } from 'mobx-react-lite';
+import { DataStoryWidget } from '../../widgets/DataStory';
 
 interface Props {
   store: Store;
@@ -11,29 +12,25 @@ const OpenModalBody: FC<Props> = ({
   store,
   clickStory,
 }) => {
-	console.log(store.metadata.demos)
   return (
     <div>
       <div className="w-full bg-gray-100 px-6 py-2">
         <div className="flex flex-col my-4 justify-center align-middle text-gray-500 text-xs">
-					<span className="my-2 font-sans font-medium text-sm text-indigo-500">
-						Your Diagrams
-					</span>
-          <ul>
-            {store.metadata.stories.map((story) => {
+          <span className="my-2 font-sans font-medium text-sm text-indigo-500">
+            Your Diagrams
+          </span>
+          <div className="py-5 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+            {store.metadata.stories.map((story, i) => {
               return (
-                <li
-                  className="my-1 hover:text-malibu-500 hover:underline cursor-pointer font-mono"
-                  key={story}
-                  onClick={() => {
-                    clickStory(story);
-                  }}
-                >
-                  {story}
-                </li>
+                <div key={`${i}-${story.name}`}>
+                  <DataStoryWidget
+                    story={story}
+                    storyLoadHandler={clickStory}
+                  />
+                </div>
               );
             })}
-          </ul>				
+          </div>
         </div>
       </div>
     </div>
