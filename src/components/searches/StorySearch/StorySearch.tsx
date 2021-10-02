da5a86431c931b907a97db0a790e05790728239f
Lenivaya <xocada@gmail.com>
2021-10-02 22:14:42 +0300

    add reusable StorySearch component

---
+++ src/components/searches/StorySearch/StorySearch.tsx
@@ -0,0 +1,66 @@
+import React, {
+  FC,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+
+import { GenericStory } from '../../../lib/types';
+import Fuse from 'fuse.js';
+
+interface Props {
+  stories: GenericStory[];
+  setSearchResult: (e) => void;
+}
+
+const searchStyle =
+  'w-full p-6 m-1 rounded appearance-none focus:outline-none focus:bg-white shadow-lg font-medium tracking-tighter antialiased';
+
+export const StorySearch: FC<Props> = ({
+  stories,
+  setSearchResult,
+}) => {
+  const [search, setSearch] = useState('');
+  const searchInput = useRef(null);
+
+  const fuse = new Fuse(stories, {
+    keys: ['name', 'description', 'tags'],
+    threshold: 0.3,
+  });
+
+  // Focus search input
+  useEffect(() => {
+    searchInput.current.focus();
+  }, []);
+
+  // Handle search changes
+  useEffect(() => {
+    search.length > 0
+      ? setSearchResult(
+          fuse.search(search).map((result) => {
+            return result.item;
+          }),
+        )
+      : setSearchResult(stories);
+  }, [search]);
+
+  const searchChange = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setSearch(e.target.value);
+  };
+
+  return (
+    <input
+      autoComplete="off"
+      id="node-search"
+      value={search}
+      onChange={searchChange}
+      type="text"
+      ref={searchInput}
+      className={searchStyle}
+      placeholder="name | description | tags ..."
+      tabIndex={1}
+    />
+  );
+};
