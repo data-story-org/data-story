56f2576f9c51aa9f690b79aa74f2497bfc9bb149
Lenivaya <xocada@gmail.com>
2021-10-03 12:12:17 +0300

    add new afterStoryClick argument to the StoryGrid

--- src/components/widgets/DataStory/StoryGrid.tsx
+++ src/components/widgets/DataStory/StoryGrid.tsx
@@ -10,18 +10,25 @@ interface Props {
   store: Store;
   stories: GenericStory[];
   isDemos?: boolean;
+  afterStoryClick?: () => void;
 }
 
 const storyGridStyle =
   'px-10 py-5 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5';
 
 export const StoryGrid: FC<Props> = observer(
-  ({ store, stories, isDemos = false }) => {
+  ({
+    store,
+    stories,
+    isDemos = false,
+    afterStoryClick = () => {},
+  }) => {
     const onClick = (name: string) => {
       isDemos
         ? loadDemo(store, name)
         : loadStory(store, name);
 
+      afterStoryClick();
       store.setPage('Workbench');
     };
 
