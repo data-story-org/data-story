f77a58e0d91433b53ff1f2f9844b5fec5c253447
Lenivaya <xocada@gmail.com>
2021-10-14 22:58:50 +0300

    add support of rendering rows in table by withRepeatable HOC

---
+++ src/components/fields/Repeatable/types.ts
@@ -0,0 +1,24 @@
+import { ComponentType } from 'react';
+import { NodeParameter } from '../../../../../core/lib/src';
+import { BaseEventHandler } from '../../../lib/types';
+import { FieldProps, RepeatableFieldProps } from '../types';
+
+export type ButtonShowPredicate = (
+  index: number,
+) => boolean;
+
+export type RepeatableRendererHandlerWrapper = (
+  repeatableIndex: number,
+) => BaseEventHandler;
+
+export interface RepeatableRendererProps {
+  Field: ComponentType<FieldProps>;
+  options: NodeParameter;
+  showAddButton: ButtonShowPredicate;
+  showRemoveButton: ButtonShowPredicate;
+  repeatablesKeys: string[];
+  repeatablesIndexes: number[];
+  handleChangeWrapper: RepeatableRendererHandlerWrapper;
+  handleRemoveButtonPress: RepeatableRendererHandlerWrapper;
+  handleAddButtonPress: RepeatableRendererHandlerWrapper;
+}
