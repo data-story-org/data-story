2eb56bc7dc2794d697c4ea0d0dfe56be0ec75c1e
Lenivaya <xocada@gmail.com>
2021-10-15 09:38:54 +0300

    use table-auto class

--- src/components/fields/Row/RowTable.tsx
+++ src/components/fields/Row/RowTable.tsx
@@ -12,7 +12,7 @@ export const RowTable: FC<Props> = ({
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
+            <table className="min-w-full table-auto divide-y divide-gray-200">
               <RowHeader columnNames={columnNames} />
               <tbody className="bg-white divide-y divide-gray-200">
                 {children}
