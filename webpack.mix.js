d7c4c4103a87d663d81c8e30a9f5cece82abfa22
Lenivaya <xocada@gmail.com>
2021-10-20 18:32:15 +0300

    add separate css's for icons and fonts

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,6 +6,8 @@ mix
   .ts('src/render.tsx', 'dist/js')
   .react()
   .sass('src/sass/app.scss', 'dist/css')
+  .sass('src/sass/icons.scss', 'dist/css')
+  .sass('src/sass/fonts.scss', 'dist/css')
   .options({
     processCssUrls: true,
     postCss: [tailwindcss('./configs/tailwind.config.js')],
