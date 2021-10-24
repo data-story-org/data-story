369b3302eaeda6ff71fc4377d95a0d1d4d1c987a
Lenivaya <xocada@gmail.com>
2021-10-24 23:09:43 +0300

    import fonts and icons by default

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,8 +6,6 @@ mix
   .ts('src/render.tsx', 'dist/js')
   .react()
   .sass('src/sass/app.scss', 'dist/css')
-  .sass('src/sass/icons.scss', 'dist/css')
-  .sass('src/sass/fonts.scss', 'dist/css')
   .options({
     processCssUrls: true,
     postCss: [tailwindcss('./configs/tailwind.config.js')],
