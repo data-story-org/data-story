ac879ad7359f70adb921de7e3a5e6c9fcdd03807
Anders Jurisoo <jurisoo@hotmail.com>
2021-11-10 22:47:39 +0100

    wip

---
+++ src/components/pages/Inspector/InspectorCanvas/InspectorCanvas.tsx
@@ -0,0 +1,20 @@
+import React, { FC, useCallback, useEffect, useRef } from 'react';
+
+export const InspectorCanvas = ({features}) => {
+  
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+
+    context.fillStyle = 'red'
+		features.forEach(feature => {
+			context.fillRect(feature.get('x'), feature.get('y'), 1, 1)
+		})
+  })
+  
+  return (<canvas className="p-4 w-full h-full" ref={canvasRef} />)
+};
+
+
