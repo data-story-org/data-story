01f467e78f5566ff04735e3c6ef43d4acedbbae0
Lenivaya <xocada@gmail.com>
2021-11-28 21:10:49 +0200

    set id's for confirmDialog buttons

--- src/lib/utils/Dialog.tsx
+++ src/lib/utils/Dialog.tsx
@@ -137,6 +137,7 @@ export const ConfirmDialog: FC<Props> = ({
             <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
               <button
                 type="button"
+                id="confirm-button"
                 className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
                 onClick={confirmationHandler}
                 ref={confirmButtonRef}
@@ -145,6 +146,7 @@ export const ConfirmDialog: FC<Props> = ({
               </button>
               <button
                 type="button"
+                id="cancel-button"
                 className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                 onClick={cancelationHandler}
                 ref={cancelButtonRef}
